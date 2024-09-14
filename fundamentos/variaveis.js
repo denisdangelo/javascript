65b367fc47ce3cb9cc6245da6acea595fbcf5529
Denis DAngelo <denis.dsilveira1@outlook.com>
2024-09-13 21:32:35 -0300

    variaveis

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,24 @@
+/**
+ * Estudo de constantes e variáveis
+ * @author: Denis D'Angelo
+ */
+
+//Limpar o console
+console.clear()
+
+//variaveis
+let nome
+let idade
+let vip
+
+nome= "Denis D'Angelo"
+console.log(typeof(nome))
+console.log(nome)
+
+idade = 56
+console.log(typeof(idade))
+console.log(idade)
+
+vip = true
+console.log(typeof(vip))
+console.log(vip)

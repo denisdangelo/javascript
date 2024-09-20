da3127e2d7512fff55b35272f32cc4c9aa735842
Denis DAngelo <denis.dsilveira1@outlook.com>
2024-09-19 22:07:02 -0300

    projeto academia

---
+++ exercicios/academia.js
@@ -0,0 +1,31 @@
+/**
+ * Ficha de um aluno de academia
+ * @author Denis D'Angelo
+ */
+
+//variaveis
+let nome
+let idade
+let peso
+let altura
+let vip
+
+console.clear()
+
+// entrada de dados
+nome = "Denis"
+idade = 31
+peso = 65
+altura = 1.69
+vip = true
+
+// saida
+//o simbolo "``" serve para concatenar, juntar. Igual ao Join no scratch
+console.log("Ficha do Aluno")
+console.log("_____________________________")
+console.log(`Nome: ${nome}`)
+console.log(`Idade: ${idade}`)
+console.log(`Peso: ${peso}`)
+console.log(`Altura: ${altura}`)
+console.log(`Vip: ${vip}`)
+

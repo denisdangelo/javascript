f5e8b402e7726c5eb0d16ae6a0d035e09dd80840
Denis DAngelo <denis.dsilveira1@outlook.com>
2024-09-24 22:22:21 -0300

    Criacao de arquivo sintexe1.js

---
+++ fundamentos/sintaxe1.js
@@ -0,0 +1,20 @@
+/**
+ * Sintaxe básica da linguagem js
+ * @Author Denis D'Angelo
+ */
+
+//Limpar a tela do console
+console.clear()
+
+//JS - Sentença e ";" opcional
+console.log("Hello World");
+
+// Concatenação - Unir textos e variaveis
+let nome = "Denis"
+console.log(`Nome:  ${nome}`)
+console.log("Nome: " ) //não recomendado
+
+// Arredondamento
+let media = 5.789
+console.log(`Media: ${media.toFixed(1)}`) // toFixed(2) fixa o valor após a cada decimal em 2 O valor dentro de parenteses determina quantas linhas 
+                                          //após a casa decimal serão exibidas

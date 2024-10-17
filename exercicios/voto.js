2aefa3de0325c3bc9e0ab719687e123d512ff3b6
Denis DAngelo <denis.dsilveira1@outlook.com>
2024-10-17 20:04:20 -0300

    Criação do arquivo voto.js que avalia se o voto é obrigatório, facultativo ou proibido a partir da validação da indade

---
+++ exercicios/voto.js
@@ -0,0 +1,24 @@
+/**
+ * Obrigatoriedade do voto
+ * @Author Denis D'Angelo
+ */
+
+
+//bibiloteca
+const input = require('readline-sync')
+const color = require('colors')
+
+//variaveis
+let idade
+
+//entrada
+idade = Number(input.question("Insira sua idade: "))
+
+//processamento * saida
+if (idade < 16) {
+    console.log("Voto proibido".red)
+} else if (idade > 17 && idade < 71) {
+    console.log("Voto obrigatório".blue)
+} else {
+    console.log("Voto facultativo".green)
+}

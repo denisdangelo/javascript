2ba8f1574642ac46a814ba74eb4e6cc691ef341c
Denis DAngelo <denis.dsilveira1@outlook.com>
2024-10-31 22:26:37 -0300

    Criação de uma calculadora em js

---
+++ htmljs/script.js
@@ -0,0 +1,10 @@
+/**
+ * Hello JS
+ * Exemplo de ligação do documento html com o JS
+ * @author Denis D'Angelo
+ */
+
+function helloJS(){
+    let nome = prompt("Digite o seu nome: ") //prompt aceita incerção de um valor 
+    alert(`Hello ${nome}`)      //alert, abre uma caixa de dialogo no navegador  
+}

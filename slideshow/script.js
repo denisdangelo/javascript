b3b2ce77e192a094634798c27a89a73e3f55d2cd
DenisDS <denis.dsilveira1@outlook.com>
2025-01-23 20:11:45 -0300

    inclusão da pasta e arquivos slideshow

--- slideshow/script.js
+++ slideshow/script.js
@@ -8,11 +8,38 @@ let slides = ["banner1.jpg", "banner2.jpg", "banner3.jpg"]
 let intervalo = 3000 //(3000ms = 3s (1s por imagem))
 let indice = 0 // apoio a lógica (índice do array)
 
+
 // Função para troca das imagens
 function show() {
     // capturar o id do banner
     const slide = document.getElementById('slide')
     
+    //Efeito de entrada
+    //A estrutura a baixo verifica se a classe 'slideOut' foi atribuido ao identificador slide
+    //'contains' verifica, 'revome' remove e 'add' adiciona a classe, respectivamente 
+    if(slide.classList.contains('slideOut')){
+        slide.classList.remove('slideOut')
+    }
+    slide.classList.add('slideIn')
+
+    //troca de imagem
+    setTimeout(() => {
+        indice++
+        if(indice >= slides.length){
+            indice = 0
+        }
+        slide.src = `img/${slide[indice]}`
+    },1000)
+
+    //Efeito de saida
+
+    if(slide.classList.contains('slideIn')){
+        slide.classList.remove('slideIn')
+    }
+    slide.classList.add('slideOut')
+
+    // Intervalo para próxima execução (loop infinito)
+    setTimeout(show, intervalo)
 }
 
 // executar a função ao iniciar o documento js
